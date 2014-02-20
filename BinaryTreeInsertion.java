1ea4b70fe4ee2e78cdaa000d54d1ac27dd452900
vishal sharma <vishal.sharma@aggiemail.usu.esu>
2014-02-20 13:30:25 -0700

    Addition of a new element to Binary tree
    Have a recursive function in which we pass the root of the tree and see
    if the value to be added is smaller or bigger than the root, and
    depending on result, we go to left or right of the tree, assuming the
    left (or right) is not null. If null, just use new to create a memory
    location, put the value, and use the left reference of the root to link
    to this new memory

---
+++ BinaryTreeInsertion.java
@@ -0,0 +1,80 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication;
+
+/**
+ *
+ * @author vishal
+ */
+class Node
+{
+    Node left;
+    int value;
+    Node right;
+    
+    Node(int n)
+    {
+        left = null;
+        value = n;
+        right = null;
+    }
+}
+
+public class BinaryTreeInsertion 
+{
+
+    
+    public static void insertNode(Node root,int n)
+    {
+        Node temp = new Node(n);
+        insert(root,temp);
+    }
+    
+    public static void insert(Node root,Node n)
+    {
+        if(root==null)
+             root=n;
+        if(root.value < n.value)
+        {
+            if(root.right==null)
+            {
+                root.right = n;
+            }
+            else
+            {
+                insert(root.right,n);
+            }
+        }
+        else
+        {
+            if(root.left == null)
+                root.left = n;
+            else
+                insert(root.left,n);
+        } 
+    }
+    
+    public static void inOrder(Node root)
+    {
+        if(root==null)
+           return;
+        
+        inOrder(root.left);
+        System.out.println(root.value);
+        inOrder(root.right);
+    }
+    
+    public static void main(String args[])
+    {
+       Node root = new Node(10);
+       insertNode(root,8);
+       insertNode(root,17);
+       insertNode(root,15);
+       insertNode(root,4);
+       insertNode(root,16);
+       
+       inOrder(root);
+    }
+}
